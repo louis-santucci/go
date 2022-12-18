7049354ef917e3d52ed84eeb8c49efa0b6a3e2aa
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-18 14:46:01 +0100

    [DATABASE][REPO INTERFACE] Fixed interface filename, and fixed param in interface definition

--- GoApp/database/repositoryInterface.go
+++ GoApp/database/repositoryInterface.go
@@ -7,7 +7,7 @@ type Repository interface {
 	Create(redirection models.Redirection) (*models.Redirection, error)
 	All() ([]models.Redirection, error)
 	GetById(id int64) (*models.Redirection, error)
-	GetByName(name string) (*models.Redirection, error)
+	GetByRedirection(redirection string) (*models.Redirection, error)
 	Update(id int64, updated models.Redirection) (*models.Redirection, error)
 	Delete(id int64) error
 }
