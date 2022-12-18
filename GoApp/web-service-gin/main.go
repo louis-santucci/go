7e0fc234c979ca39a81ae3604e9219aad9d9f306
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-18 13:13:57 +0100

    [MAIN][DEFAULT] Implemented default main for test purposes

--- GoApp/web-service-gin/main.go
+++ GoApp/web-service-gin/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"go.com/models"
+	"net/http"
 	"time"
 )
 
 var redirections = []models.Redirection{
-	{ID: 1, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now()},
-	{ID: 2, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now()},
-	{ID: 3, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now()},
-	{ID: 4, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now()},
-	{ID: 5, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now()},
+	{ID: 1, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now(), VIEWS: 0},
+	{ID: 2, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now(), VIEWS: 5},
+	{ID: 3, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now(), VIEWS: 3},
+	{ID: 4, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now(), VIEWS: 2},
+	{ID: 5, SHORTCUT: "fb", REDIRECT_URL: "https://facebook.com", CREATED_AT: time.Now(), VIEWS: 20},
+}
+
+func getRedirections(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, redirections)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/redirections", getRedirections)
+	router.Run("localhost:9090")
 }
