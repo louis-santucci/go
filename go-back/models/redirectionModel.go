4131b663f8a2afb5e68f4b99276c1f23dd34b360
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-18 21:07:15 +0100

    [REDIRECTION] Refactored inputs into one class

--- go-back/models/redirectionModel.go
+++ go-back/models/redirectionModel.go
@@ -7,23 +7,15 @@ import (
 type Redirection struct {
 	ID          uint      `json:"id" gorm:"primarykey"`
 	Shortcut    string    `json:"shortcut" gorm:"unique;not null"`
-	RedirectUrl string    `json:"redirect_url" gorm:"not null"'`
+	RedirectUrl string    `json:"redirect_url" gorm:"not null"`
 	Views       int64     `json:"views" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"created_at;not null"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"updated_at"`
 }
 
-type RedirectionCreationInput struct {
-	Shortcut    string    `json:"shortcut"`
-	RedirectUrl string    `json:"redirect_url"`
-	CreatedAt   time.Time `json:"created_at"`
-}
-
-type RedirectionUpdateInput struct {
-	Shortcut    string    `json:"shortcut"`
-	RedirectUrl string    `json:"redirect_url"`
-	Views       int64     `json:"views"`
-	UpdatedAt   time.Time `json:"updated_at"`
+type RedirectionInput struct {
+	Shortcut    string `json:"shortcut" binding:"required"`
+	RedirectUrl string `json:"redirect_url" binding:"required"`
 }
 
 type RedirectionIncrement struct {
