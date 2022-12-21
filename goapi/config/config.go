c78d175f786c8c7fb4e0e1f05e55c82bb3a3a118
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-21 15:49:51 +0100

    [CONFIG] Added FIXME for JWT secret key, to be changed in the future for security purposes

--- goapi/config/config.go
+++ goapi/config/config.go
@@ -1,4 +1,4 @@
 package config
 
 const Db_filename = "go-db.db"
-const Jwt_secret = "ihckoxoezxhzbkxgecieruccuohexkzxknezjg"
+const Jwt_secret = "ihckoxoezxhzbkxgecieruccuohexkzxknezjg" //FIXME TO be changed for security purposes
