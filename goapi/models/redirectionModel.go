af1b1ece6428fdc880c17a34e542e6fb1eaa6490
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-23 21:06:36 +0100

    [GOAPI][REDIRECTION MODEL] fixed type of view which is now a unsigned int

--- goapi/models/redirectionModel.go
+++ goapi/models/redirectionModel.go
@@ -8,7 +8,7 @@ type Redirection struct {
 	ID          uint      `json:"id" gorm:"primarykey"`
 	Shortcut    string    `json:"shortcut" gorm:"unique;not null"`
 	RedirectUrl string    `json:"redirect_url" gorm:"not null"`
-	Views       int64     `json:"views" gorm:"not null"`
+	Views       uint      `json:"views" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"created_at;not null"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"updated_at"`
 }
@@ -16,8 +16,4 @@ type Redirection struct {
 type RedirectionInput struct {
 	Shortcut    string `json:"shortcut" binding:"required"`
 	RedirectUrl string `json:"redirect_url" binding:"required"`
-}
-
-type RedirectionIncrement struct {
-	Views int64 `json:"views"`
 }
