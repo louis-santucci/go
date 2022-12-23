ccf3e502454bcc3c3400a6a166ccbe6921c220e8
Louis SANTUCCI <louissantucci1998@gmail.com>
2022-12-23 21:50:34 +0100

    [GOAPI][RESPONSES] Updated responses' status type to int

--- goapi/responses/httpResponseModel.go
+++ goapi/responses/httpResponseModel.go
@@ -3,21 +3,21 @@ package responses
 import "net/http"
 
 type OKResponse struct {
-	Status uint        `json:"status"`
+	Status int         `json:"status"`
 	Data   interface{} `json:"data"`
 }
 
 type ErrorResponse struct {
-	Status uint        `json:"status"`
+	Status int         `json:"status"`
 	Error  interface{} `json:"error"`
 }
 
 type JWTResponse struct {
-	Status uint   `json:"status"`
+	Status int    `json:"status"`
 	Token  string `json:"jwt_token"`
 }
 
-func NewErrorResponse(status uint, error interface{}) ErrorResponse {
+func NewErrorResponse(status int, error interface{}) ErrorResponse {
 	return ErrorResponse{
 		Status: status,
 		Error:  error,
@@ -31,7 +31,7 @@ func NewOKResponse(data interface{}) OKResponse {
 	}
 }
 
-func NewJWTResponse(status uint, token string) JWTResponse {
+func NewJWTResponse(status int, token string) JWTResponse {
 	return JWTResponse{
 		Status: status,
 		Token:  token,
