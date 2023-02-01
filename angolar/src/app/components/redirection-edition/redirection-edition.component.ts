7c167d2f8253b8e5ff9f156ea196361e23d45bf9
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-02-01 18:48:10 +0100

    [ANGOLAR][REDIRECTION EDITION] Created new component

---
+++ angolar/src/app/components/redirection-edition/redirection-edition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redirection-edition',
+  templateUrl: './redirection-edition.component.html',
+  styleUrls: ['./redirection-edition.component.css']
+})
+export class RedirectionEditionComponent {
+
+}
