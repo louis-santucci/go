7e34d38845cdc6bc8c7a43445a5eddb2b88b2faf
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-02-01 18:48:04 +0100

    [ANGOLAR][REDIRECTION CREATION] Created new component

---
+++ angolar/src/app/components/redirection-creation/redirection-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redirection-creation',
+  templateUrl: './redirection-creation.component.html',
+  styleUrls: ['./redirection-creation.component.css']
+})
+export class RedirectionCreationComponent {
+
+}
