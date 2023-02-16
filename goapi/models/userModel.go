350f92ee42af3a6f93d052d16dc197a57733162a
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-02-16 20:29:31 +0100

    [GOAPI][USER MODEL] Added ID to UserInfo structure

--- goapi/models/userModel.go
+++ goapi/models/userModel.go
@@ -26,6 +26,7 @@ type UserLogin struct {
 }
 
 type UserInfo struct {
+	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
