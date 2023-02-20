7f85476486ed5777d269b0d3148cce1b02761f54
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-02-20 18:38:05 +0100

    [ANGOLAR][ROUTING UTILS] Created utils for redirection

---
+++ angolar/src/app/utils/routing-utils.spec.ts
@@ -0,0 +1,7 @@
+import { RoutingUtils } from './routing-utils';
+
+describe('RoutingUtils', () => {
+  it('should create an instance', () => {
+    expect(new RoutingUtils()).toBeTruthy();
+  });
+});
