98b12ea93415930d092b1e92f63e7ad8b6a5e9ea
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-03-10 23:25:39 +0100

    [ANGOLAR][UTILS] Removed map utils, and updated routing utils

--- angolar/src/app/utils/routing-utils.ts
+++ angolar/src/app/utils/routing-utils.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 export class RoutingUtils {
   private static NOT_FOUND_URL = '/error/notFound';
   private static UNAUTHORIZED_URL = '/error/unauthorized';
+  private static HISTORY_URL = '/history';
   private static ROOT = '/';
 
   public static goToHomepage(router: Router): void {
@@ -15,5 +16,9 @@ export class RoutingUtils {
 
   public static goToUnauthorizedPage(router: Router): void {
     router.navigateByUrl(RoutingUtils.UNAUTHORIZED_URL);
+  }
+
+  public static goToHistoryPage(router: Router): void {
+    router.navigateByUrl(RoutingUtils.HISTORY_URL);
   }
 }
