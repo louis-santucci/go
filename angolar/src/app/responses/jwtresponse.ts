451ce41c65dc41286c57ec793260f743676d698c
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-04-26 23:18:55 +0200

    [ANGOLAR] Updated JWT response

--- angolar/src/app/responses/jwtresponse.ts
+++ angolar/src/app/responses/jwtresponse.ts
@@ -1,6 +1,7 @@
 import {Response} from "./response";
+import {UserInfo} from "../models/user-info";
 
 export interface JWTResponse extends Response {
   token: string;
-  email: string;
+  user: UserInfo;
 }
