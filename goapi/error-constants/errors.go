c1419fd12fb890660435ae5013d1901ba8e09e81
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-04-26 21:26:15 +0200

    [GOAPI] Added error in errors constants

--- goapi/error-constants/errors.go
+++ goapi/error-constants/errors.go
@@ -3,3 +3,4 @@ package error_constants
 const NotFoundError string = "NOT FOUND"
 const ForbiddenError string = "FORBIDDEN"
 const UnauthorizedError string = "UNAUTHORIZED"
+const WrongRedirectionFormatError = "Wrong redirection format"
